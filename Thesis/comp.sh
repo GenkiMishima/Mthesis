168b8d5aeb4f9a096205923f6c3b5200e375bc70
三島源生 <Gmishima@mishimagenseis-iMac.local>
2015-12-21 21:22:44 +0900

    Chap2

--- Thesis/comp.sh
+++ Thesis/comp.sh
@@ -4,7 +4,14 @@ name=Main
 mv tmp/* .
 platex $name.tex
 dvipdfmx $name.dvi
-qlmanage -p $name.pdf
+if [ "$(uname)" == 'Darwin' ]; then
+  qlmanage -p $name.pdf
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  gimp $name.pdf
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
 #mv *.dvi tmp
 #mv *.aux tmp
 #mv *.log tmp
