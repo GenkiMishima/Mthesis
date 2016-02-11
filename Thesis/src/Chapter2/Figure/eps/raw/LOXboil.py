dcf9f6612f99fabe4e1d592db7351f01534d0515
三島源生 <Gmishima@mishimagenseis-iMac.local>
2016-02-11 18:46:12 +0900

    2

--- Thesis/src/Chapter2/Figure/eps/raw/LOXboil.py
+++ Thesis/src/Chapter2/Figure/eps/raw/LOXboil.py
@@ -1,7 +1,22 @@
 #/usr/local/python
 import numpy as np
 import scipy as sp
+import csv
+import matplotlib.pyplot as plt
 
+OUT = open('result.dat','w')
+csvOUT = csv.writer(OUT)
 P = np.arange(0.1,3,0.01) #[MPaA]
+for i in P:
+	T = -0.0915048213719274*i**6+1.56286330390285*i**5-10.5253049169918*i**4+35.7152007329346*i**3-65.8437773403561*i**2+75.2683477610502*i+83.9209685806225#-273.15
 
-#print T
+	Data = np.array([i,T])
+	csvOUT.writerow(Data)
+#print P,T
+#plt.plot(P,T)
+#plt.grid()
+#plt.legend(('BoilingPoint'),'upper right')
+#plt.xlabel('Pressure[MPaA]')
+#plt.ylabel('Temperature[K]')
+#plt.savefig('LOXBoil.eps')
+#plt.show()
